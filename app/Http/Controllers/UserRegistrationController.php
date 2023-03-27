969568d1d1e18be60f52289f1eb70ca238a2c797
himalMajumder <himalmajumder@gmail.com>
2023-03-27 14:20:50 +0600

    With Single Responsibility Principle

--- app/Http/Controllers/UserRegistrationController.php
+++ app/Http/Controllers/UserRegistrationController.php
@@ -8,39 +8,39 @@ use Illuminate\Http\Request;
 use App\Models\User;
 
 class UserRegistrationController extends Controller {
-    // /**
-    //  * Store a newly created user in storage
-    //  *
-    //  * @param StoreUserRequest $request
-    //  * @param UserRepository $userRepository
-    //  * @return \Illuminate\Http\RedirectResponse
-    //  */
-    // public function store(StoreUserRequest $request, UserRepository $userRepository) {
-    //     $userRepository->create($request);
-
-    //     return redirect()->back()->with('success', 'User create successfully');
-    // }
-
     /**
      * Store a newly created user in storage
      *
-     * @param Illuminate\Http\Request $request
+     * @param StoreUserRequest $request
      * @param UserRepository $userRepository
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request) {
-
-        $validate = $request->validate([
-            'name'     => 'required|string',
-            'email'    => 'required|email|regex:/(.+)@(.+)\.(.+)/i',
-            'password' => 'required|min:6',
-        ]);
-        $user           = new User();
-        $user->name     = $validate->name;
-        $user->email    = $validate->email;
-        $user->password = bcrypt($validate->password);
-        $user->save();
+    public function store(StoreUserRequest $request, UserRepository $userRepository) {
+        $userRepository->create($request);
 
         return redirect()->back()->with('success', 'User create successfully');
     }
+
+    // /**
+    //  * Store a newly created user in storage
+    //  *
+    //  * @param Illuminate\Http\Request $request
+    //  * @param UserRepository $userRepository
+    //  * @return \Illuminate\Http\RedirectResponse
+    //  */
+    // public function store(Request $request) {
+
+    //     $validate = $request->validate([
+    //         'name'     => 'required|string',
+    //         'email'    => 'required|email|regex:/(.+)@(.+)\.(.+)/i',
+    //         'password' => 'required|min:6',
+    //     ]);
+    //     $user           = new User();
+    //     $user->name     = $validate->name;
+    //     $user->email    = $validate->email;
+    //     $user->password = bcrypt($validate->password);
+    //     $user->save();
+
+    //     return redirect()->back()->with('success', 'User create successfully');
+    // }
 }
