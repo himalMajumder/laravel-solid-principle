55591e882ff10b2f8e2abe31ded9afc06b4b10c9
himalMajumder <himalmajumder@gmail.com>
2023-04-01 15:39:06 +0600

    With Interface segregation principle Report Interface break into many small specific interface

---
+++ app/Interface/ReportDataInferface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interface;
+
+interface ReportDataInterface{
+    // Separate getData Function from Report Interface
+
+    /**
+     * Get Report Data
+     *
+     * @return void
+     */
+    public function getData();
+}
--- app/Interface/ReportInterface.php
+++ app/Interface/ReportInterface.php
@@ -3,6 +3,9 @@
 namespace App\Interface;
 
 interface ReportInterface{
+    // This reportInterface should depends on all function which is not right way;
+    // Replace this fat interface with many small, specific interface;
+
     /**
      * Get Report Data
      *
